619bcbace9a4cad88d448d947e6777d155680f23
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-01-13 17:54:20 +0900

    🔧 index.js faq 데이터 추가

--- src/data/index.js
+++ src/data/index.js
@@ -2,12 +2,14 @@ import projects from "./projects.json";
 import members from "./members.json";
 import intercollegiates from "./intercollegiates.json";
 import curriculums from "./curriculums.json";
+import faq from "./faq.json";
 
 export {
   projects,
   members,
   intercollegiates,
   curriculums,
+  faq,
 };
 
 //사용하는 페이지에서 import { projects, members } from "@/data"; 이렇게 불러와서 사용
